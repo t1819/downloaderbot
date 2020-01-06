e81df788af19d5689883f4288777d3f1489f5271
Ritesh Singh <ritesh9singh@gmail.com>
2020-01-06 18:15:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def system_default():
     packages = ['curl', 'ffmpeg', 'tor', 'git', 'python']
     for i in packages:
-        cmd = "pkg install {0}".format(i)
+        cmd = "pkg install {0} -y".format(i)
         os.system(cmd)
     os.system('termux-setup-storage')
     os.system('pip install -r python2_requirements.txt')
